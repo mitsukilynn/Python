2d4ff30e1372a9f13f4d4d9e6c0c6f237de6b546
mitsukilynn <105295678+mitsukilynn@users.noreply.github.com>
2022-05-14 12:50:47 +0800

    Update e456.py

--- e456.py
+++ e456.py
@@ -1,7 +1,6 @@
-a=input().split()
+A=input().split()
+B=' '+A[len(A)-1]
 s=''
-for i in a:
-    s=i
-    for j in range(3):
-        #if(len(s)>=3):
-        print( i[0]+' little, '+i[1]+' little, '+i[2]+' little Indians')
+for i in A[0:len(A)-1]:
+    s=s+' '+i+' little,'
+print(s[0:len(s)]+B+" little Indians")
